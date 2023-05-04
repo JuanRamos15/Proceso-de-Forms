ccd1452fb7f4b5d383482498788217507abdd196
JuanRamos15 <124464419+JuanRamos15@users.noreply.github.com>
2023-05-04 19:17:21 +0000

    read html📖

---
+++ app.js
@@ -0,0 +1,126 @@
+import path from "path";
+import { promises as fs } from 'fs';
+export default async (req, res) => {
+    // Desestructurando de "req"
+    let { url, method } = req;
+
+    console.log(`📣 CLIENT-REQUEST: ${req.url} ${req.method}`);
+
+    // Enrutando peticiones
+    switch (url) {
+        case '/':
+            const readHtml = path.join(__dirname, 'index.html');
+            try {
+                const data = await fs.readFile(faviconPath);
+                res.writeHead(200, { 'Content-Type': 'image/x-icon' });
+                res.end(data);
+            } catch (err) {
+
+
+                // Peticion raiz
+                // Estableciendo cabeceras
+                res.setHeader('Content-Type', 'text/html');
+                // Escribiendo la respuesta
+                res.write('');
+                console.log(`📣 Respondiendo: 200 ${req.url} ${req.method}`);
+                // Estableciendo codigo de respuesta
+                res.statusCode = 200;
+                // Cerrando la comunicacion
+                res.end();
+            }
+            break;
+        case '/author':
+            // se crea el nuevo case para poder entrar a author
+            // Estableciendo cabeceras
+            res.setHeader('Content-Type', 'text/html');
+            // Escribiendo la respuesta
+            res.write(``);
+            console.log(`📣 Respondiendo: Author ${req.url} ${req.method}`);
+            // Estableciendo codigo de respuesta
+            res.statusCode = 215;
+            // Cerrando la comunicacion
+            res.end();
+            break;
+        case "/favicon.ico":
+            // Especificar la ubicación del archivo de icono
+            const faviconPath = path.join(__dirname, 'favicon.ico');
+            try {
+                const data = await fs.readFile(faviconPath);
+                res.writeHead(200, { 'Content-Type': 'image/x-icon' });
+                res.end(data);
+            } catch (err) {
+                console.error(err);
+                // Peticion raiz
+                // Estableciendo cabeceras
+                res.setHeader('Content-Type', 'text/html');
+                // Escribiendo la respuesta
+                res.write(``);
+                console.log(`📣 Respondiendo: 500 ${req.url} ${req.method}`);
+                console.log(`error 500 ${err.message}`)
+                // Estableciendo codigo de respuesta
+                res.statusCode = 500;
+                // Cerrando la comunicacion
+                res.end();
+            }
+            break;
+        case "/message":
+            // Verificando si es post
+            if (method === "POST") {
+                // Se crea una variable para almacenar los
+                // Datos entrantes del cliente
+                // Se registra un manejador de eventos
+                // Para la recepción de datos
+                let body = "";
+                req.on("data", (data => {
+                    body += data;
+                    if (body.length > 1e6) return req.socket.destroy();
+                }));
+                // Se registra una manejador de eventos
+                // para el termino de recepción de datos
+                req.on("end", async () => {
+                    // Procesa el formulario
+
+                    // Mediante URLSearchParams se extraen
+                    // los campos del formulario
+                    const params = new URLSearchParams(body);
+                    // Se construye un objeto a partir de los datos
+                    // en la variable params
+                    const parsedParams = Object.fromEntries(params);
+                    // Se finaliza la conexion
+                    fs.writeFile('message.txt', 'parsedParams.message');
+                })
+                res.statusCode = 302;
+                res.setHeader('Location', '/')
+                return res.end();
+            } else {
+                res.statusCode = 404;
+                res.write("404: Endpoint no encontrado")
+                res.end();
+            }
+            break;
+        // Continua con el defautl
+        default:
+            // Peticion raiz
+            // Estableciendo cabeceras
+            res.setHeader('Content-Type', 'text/html');
+            // Escribiendo la respuesta
+            res.write(`
+        <html>
+          <head>
+            <link rel="icon" type="image/x-icon" sizes="32x32" href="/favicon.ico">
+            <title>My App</title>
+          </head>
+          <body> 
+            <h1>&#128534; 404 Recurso no encontrado</h1>
+            <p>Lo sentimos pero no tenemos lo que buscas...</p>
+          </body>
+        </html>
+        `);
+            console.log(`📣 Respondiendo: 404 ${req.url} ${req.method}`);
+            // Estableciendo codigo de respuesta
+            res.statusCode = 404;
+            // Cerrando la comunicacion
+            res.end();
+            break;
+    }
+}
